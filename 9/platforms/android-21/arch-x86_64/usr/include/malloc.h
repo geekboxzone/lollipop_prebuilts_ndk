b8a3ce7833113135d7ce573aa52433351bb6e14b
Andrew Hsieh <andrewhsieh@google.com>
2014-10-21 16:32:42 -0700

    Add real android-21 32-bit headers/libs
    Also corret __ANDROID_API__ in 64-bit android/api-level.h
    
    Change-Id: Ia7f82f07d70eebb2daa62e03c227091b74d05fb6

--- 9/platforms/android-21/arch-x86_64/usr/include/malloc.h
+++ 9/platforms/android-21/arch-x86_64/usr/include/malloc.h
@@ -27,19 +27,12 @@
 
 __BEGIN_DECLS
 
-#if !defined(__clang__) || __clang_major__ > 3 || (__clang_major__ == 3 && __clang_minor__ < 5)
 extern void* malloc(size_t byte_count) __mallocfunc __wur __attribute__((alloc_size(1)));
 extern void* calloc(size_t item_count, size_t item_size) __mallocfunc __wur __attribute__((alloc_size(1,2)));
 extern void* realloc(void* p, size_t byte_count) __wur __attribute__((alloc_size(2)));
-extern void* memalign(size_t alignment, size_t byte_count) __mallocfunc __wur __attribute__((alloc_size(2)));
-#else
-extern void* malloc(size_t byte_count) __mallocfunc __wur;
-extern void* calloc(size_t item_count, size_t item_size) __mallocfunc __wur;
-extern void* realloc(void* p, size_t byte_count) __wur;
-extern void* memalign(size_t alignment, size_t byte_count) __mallocfunc __wur;
-#endif
-
 extern void free(void* p);
+
+extern void* memalign(size_t alignment, size_t byte_count) __mallocfunc __wur __attribute__((alloc_size(2)));
 extern size_t malloc_usable_size(const void* p);
 
 #ifndef STRUCT_MALLINFO_DECLARED
